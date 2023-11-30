6056b3a6efdeb63bd085d146becbf251da8d6021
Rafael Dametto <cacotpds@gmail.com>
2023-11-30 16:15:13 -0300

    anatomia de classes 1-2-3

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        String primeiroNome = "Rafael";
+        String segundoNome = "Fortunato Dametto";
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        int anoFabricacao = 2018;
+        boolean verdadeFalso = true;
+
+        System.out.println(primeiroNome +" " + anoFabricacao +" "+ verdadeFalso);
+
+        System.out.println(nomeCompleto);
+    }
+    public static String nomeCompleto(String primeiroNome, String segundoNome){
+        return primeiroNome.concat(" ").concat(segundoNome);
     }
 }
